1476f02054efbeb6043eadd180ae8544b792fe6c
Darja <darja.peternel@gmail.com>
2017-11-22 20:53:41 +0100

    Trying to parse lines

--- script2.sh
+++ script2.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-months=('Enero',01 'Febrero',02 'Marzo',03 'Abril',04 'Mayo',05 'Junio',06 'Julio',07 'Agosto',08 'Septiembre',09 'Octubre',10 'Noviembre',11 'Diciembre',12)
 for year in 2006
 do
-	for month in "${months[@]}"
+	for month in 01 # 02 03 04 05 06 07 08 09 10 11 12
 	do 
-		IFS=","; set -- $month  # $1 == enero, $2 == 01
-		f=$(find ${year}/${1} -name '*.txt')
-		echo "$f"
+		
+		cat $(find ${year}/${month}/ -name '*.txt') | tail -54 | head -52 > f.txt
+		
+		cat f.txt | while read -r line
+		do
+			cat "$line" | awk -F'  '+ '{print $2}' # | awk -F. '{print $$1$2$3}' "$line" >> compustibleNuclear.txt
+		done 
+				
 	done
 done
