46e1e0bbe33b30fc1d8ea971f8276ebe06edd92a
Darja <darja.peternel@gmail.com>
2017-11-24 13:06:58 +0100

    Added a for loop for creating .txt files and made code shorter

--- script2.sh
+++ script2.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
+#The main idea of the last script is to read the .txt files for each month of the year and create other .txt file named
+#after the column in the same .txt file. Lines of this file contain data about Provinicias and its expenses when using fuel, carbones,..
+#Knowing this data and having a structure of the relation tree between ciudades autonomas and provincias the script writes 
+#data of the year and month and the emount of particular expense in a file named after it and puts it in the apropriate directory.
+#So at the end each directory of provinacias in Spain will contain 7 .txt files named compustibleNuclear, carbones,.. 
+#whose contents will look like this: 
+#200601 704350
+#200602 645577
+#200603 566475
+#200604 533406
+#200605 615843
+#200606 568253
+#200607 778101
 
-for year in 2006
+for year in {2006..2014}
 do
-	for month in 01 # 02 03 04 05 06 07 08 09 10 11 12
+	for month in 01 02 03 04 05 06 07 08 09 10 11 12
 	do 
 		cat $(find ${year}/${month}/ -name '*.txt') | tail -54 | head -52 > f.txt
-		
 		cat f.txt | while read -r line
 		do
-			yearMonth="${year}${month}"
-			proName=$(echo "$line" | awk -F'  '+ '{print $1}' | sed 's/ /_/g')
-			val=$(echo "$line" | awk -F'  '+ '{print $2}' | awk -F. '{print $1$2$3}') 
-			file=$(find spain -name "*/*${proName}")
-			#echo "$file"
-			p="${yearMonth} ${val}"
-			echo "$p" >>  "${file}/compustibleNuclear.txt"
+			for i in compustibleNuclear,2 carbones,3 lignitos,4 fuel,5 gasNatural,6 otrosCompustibles,7 total,8
+			do
+				IFS=","; set -- $i  # $1 == compustiblesNuclear, $2 == 2
+				proName=$(echo "$line" | awk -F'  '+ '{print $1}' | sed 's/ /_/g'| sed 's/Ñ/N/g')
+				echo "$line" | awk -v col=${2} -F'  '+ '{print $col}' | awk -F' ' '{print $1}' | awk -v yearMonth="${year}${month}" -F. '{print yearMonth,$1$2$3}' >> $(find spain/*/* -name "*"$proName"")/${1}.txt
+			done
 		done 		
 	done
 done
